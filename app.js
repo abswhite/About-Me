1b42caf524feb10d20c381111b66521b7a2ebc9c
‘abswhite1git config --global user.email ‘abigailwhite213@gmail.com <abigailwhite213@gmail.comgit config --global user.name abswhite1>
2017-01-03 16:33:57 -0800

    Cleaned up JS console.log and created css divs

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict';
 // first line every time
 
-var answerOne = confirm('Are you ready?');
+var answerConfirm = confirm('Are you ready?');
 
-if(answerOne) {
+if(answerConfirm) {
   console.log('Woo! User is ready!');
 } else {
   console.log('Sorry, too bad....');
@@ -15,51 +15,71 @@ var name = prompt('What is your name?');
 console.log('The user\'s name is ' + name);
 
 //Question One
-var answerOne = prompt(name + ', this is a yes or no question, so please answer with a Y or N. Is Abigail\'s middle name is Margaret?').toLowerCase();
-console.log('The user answered ' + answerOne);
+var answerOne = prompt(name + ', this is a yes or no question, so please answer with a Y or N. Is Abigail\'s middle name is Margaret?').toUpperCase();
+if(answerOne === 'N') {
+  console.log('Correct. For Question One, the user answered ' + answerOne + '.');
+} else {
+  console.log('Incorrect. For Question One, the user answered ' + answerOne + '. The answer is N.');
+}
 
-if (answerOne === 'n') {
-  alert('Good guess! Her middle name is Lauren, like Lauren Conrad, from The Hills, which I love');
+if (answerOne === 'N') {
+  alert('Good guess! Her middle name is Lauren, like Lauren Conrad, from The Hills, which she loves.');
 } else {
   alert('Wrong but on the right track. Like her first name and last name, Abigail\'s middle name is painfully unidentifying and white. ' + 'Her name is Lauren.');
 }
 
 //Question Two
-var answerTwo = prompt(name + ', here\'s another yes or no question. Did Abigail live in the UK last year?').toLowerCase();
-console.log('The user answered ' + answerTwo);
+var answerTwo = prompt(name + ', here\'s another yes or no question. Did Abigail live in the UK last year?').toUpperCase();
+if(answerTwo === 'Y') {
+  console.log('Correct. For Question Two, the user answered ' + answerTwo + '.');
+} else {
+  console.log('Incorrect. For Question Two, the user answered ' + answerTwo + '. The answer is Y.');
+}
 
-if (answerTwo === 'y') {
-  alert('True! The trauma of 20 years of Boston winters finally took its toll and I fled back to the motherland. I lived in London for 10 months while completing my junior year.');
+if (answerTwo === 'Y') {
+  alert('True! The trauma of 20 years of Boston winters finally took its toll and so she fled back to the motherland. She lived in London studying at LSE for 10 months while completing her junior year.');
 } else {
   alert('Okay. I mean, you\'re wrong. But I still respect you.');
 }
 
 //Question Three
-var answerThree = prompt(name + 'You know the drill ,' + name + '. Y or N. Does Abigail enjoy gardening?').toLowerCase();
-console.log('The user answered ' + answerThree);
+var answerThree = prompt('You know the drill, ' + name + '. Y or N. Does Abigail enjoy gardening?').toUpperCase();
+if(answerOne === 'N') {
+  console.log('Correct. For Question Three, the user answered ' + answerThree + '.');
+} else {
+  console.log('Incorrect. For Question Three, the user answered ' + answerThree + '. The answer is N.');
+}
 
-if (answerThree === 'n') {
-  alert('Nice. You\'re right. I like lots of outdoorsy things, but sitting in the sun getting eaten alive by bugs is not one of them!');
+if (answerThree === 'N') {
+  alert('Nice. You\'re right. She likes lots of outdoorsy things, but sitting in the sun getting eaten alive by bugs is not one of them!');
 } else {
   alert('Lol! Wrong.');
 }
 
 //Question Four
-var answerFour = prompt(name + 'Y or N. Abigail has a dog named Hobbes.').toLowerCase();
-console.log('The user answered ' + answerFour);
-
-if (answerFour === 'y') {
-  alert('True! My dog is named either after Thomas Hobbes (morbid philospher) or Hobbes (cute comic book tiger). Who\'s to say which one.');
+var answerFour = prompt(name + ', Y or N. Does Abigail have a dog, just as Adam does?').toUpperCase();
+if(answerFour === 'Y') {
+  console.log('Correct. For Question Four, the user answered ' + answerFour + '.');
 } else {
-  alert('How dare you.');
+  console.log('Incorrect. For Question Four, the user answered ' + answerOne + '. The answer is Y.');
+}
+
+if (answerFour === 'Y') {
+  alert('True! Her dog is named either after Thomas Hobbes (boring, morbid philospher) or Hobbes (cute comic book tiger). Who\'s to say which one.');
+} else {
+  alert('How DARE you. Of COURSE she has a dog. Get off this website.');
 }
 
 //Question Five
-var answerFive = prompt('LAST ONE ' + name + '! Y or N, please. Does Abigail enjoy yoga?').toLowerCase();
-console.log('The user answered ' + answerFive);
+var answerFive = prompt('Last one ' + name + '! Y or N, please. Does Abigail enjoy yoga?').toUpperCase();
+if(answerFive === 'Y') {
+  console.log('Correct. For Question Five, the user answered ' + answerFive + '.');
+} else {
+  console.log('Incorrect. For Question Five, the user answered ' + answerFive + '. The answer is Y.');
+}
 
-if (answerFive === 'y') {
-  alert('Yes! I mean, I\'m terrible, but I practice 5-6 times a week. Pls let me know if you know any good studios int he area.');
+if (answerFive === 'Y') {
+  alert('Yes! I mean, She\'s terrible, but she practices 5-6 times a week. Pls let her know if you know any good studios in the area.');
 } else {
   alert('Wrong. You lost.');
 }
